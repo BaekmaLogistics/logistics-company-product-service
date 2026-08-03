f76dd18d50008914806bfb1277d25f33908db6fe
shak4168 <shak2168@gmail.com>
2026-08-03 15:53:38 +0900

    Template 복제 및 기본 세팅

--- src/main/java/com/sparta/logistics/CompanyServiceApplication.java
+++ src/main/java/com/sparta/logistics/CompanyServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LogisticsApplication {
+public class CompanyServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LogisticsApplication.class, args);
+        SpringApplication.run(CompanyServiceApplication.class, args);
     }
 
 }
