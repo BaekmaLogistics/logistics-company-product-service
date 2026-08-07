fc4d00d365125de8e537dc5da2405e4de4b78b7c
shak4168 <shak2168@gmail.com>
2026-08-07 12:57:45 +0900

    fix:import 정리

--- src/main/java/com/sparta/logistics/presentation/query/controller/CompanyInternalController.java
+++ src/main/java/com/sparta/logistics/presentation/query/controller/CompanyInternalController.java
@@ -1,16 +1,10 @@
 package com.sparta.logistics.presentation.query.controller;
 
 import com.sparta.logistics.application.query.dto.company.CompanyDetailResponseDto;
-import com.sparta.logistics.application.query.dto.company.CompanyListResponseDto;
-import com.sparta.logistics.application.query.dto.company.CompanySearchRequestDto;
-import com.sparta.logistics.application.query.usecase.company.GetCompanyListUseCase;
 import com.sparta.logistics.application.query.usecase.company.GetCompanyUseCase;
 import com.sparta.logistics.common.code.GeneralResponseCode;
 import com.sparta.logistics.presentation.common.dto.response.GeneralResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/sparta/logistics/presentation/query/controller/ProductInternalController.java
+++ src/main/java/com/sparta/logistics/presentation/query/controller/ProductInternalController.java
@@ -1,16 +1,10 @@
 package com.sparta.logistics.presentation.query.controller;
 
 import com.sparta.logistics.application.query.dto.product.ProductDetailResponseDto;
-import com.sparta.logistics.application.query.dto.product.ProductListResponseDto;
-import com.sparta.logistics.application.query.dto.product.ProductSearchRequestDto;
-import com.sparta.logistics.application.query.usecase.product.GetProductListUseCase;
 import com.sparta.logistics.application.query.usecase.product.GetProductUseCase;
 import com.sparta.logistics.common.code.GeneralResponseCode;
 import com.sparta.logistics.presentation.common.dto.response.GeneralResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
